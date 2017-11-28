f968554f736ccfa3a4f28f33ac81f87fb68664b8
jlshm <1182728695@qq.com>
2017-11-28 11:28:29 +0800

    first commit

---
+++ parent/hello-world/src/main/java/com/learn/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.learn;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        User user = new User("lin");
+        System.out.println("Hello World  :" + user.getName());
+    }
+}
---
+++ parent/user/src/main/java/com/learn/User.java
@@ -0,0 +1,21 @@
+package com.learn;
+
+public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public User() {
+        super();
+    }
+}
